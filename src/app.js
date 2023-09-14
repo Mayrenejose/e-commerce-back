f512f1e2254b09ab5d2937ccb8dc5fd7de4b4c5e
mayrene <maye199226@gmail.com>
2023-09-14 11:15:28 -0300

    solving cors problem

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require("cors")
 const endpointsEcommerce = require('./routes')
 
 const app = express()
-app.use(express.json())
-const PORT = process.env.REACT_APP_ENV || 3000
+//app.use(express.json())
+app.use(cors({ origin: 'http://localhost:3000'}))
+const PORT = process.env.REACT_APP_ENV || 3001
 
 app.use('/api/items', endpointsEcommerce)
 
